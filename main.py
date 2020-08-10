f14374cbf2df0255f10a826eeb19694b4d75e678
Fahreza <fahreza_12@apps.ipb.ac.id>
2020-08-10 12:11:57 +0700

    create enroll_sc function

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import WebDriverException
+from selenium.webdriver.support.ui import Select
+import numpy as np
+
+import time
+import re
 
 # constants
 # path to webdriver
 BASE_URL = "https://krs.simak.ipb.ac.id"
 PATH = 'C:\Program Files (x86)\msedgedriver.exe'
-USERNAME = ''
-PASSWORD = ''
+USERNAME = 'fahreza_12'
+PASSWORD = '279654066abc'
+COURSE_IDS = [
+    'IKK233',
+    'EKO201',
+]
 
 # initialize webdriver
 driver = webdriver.Edge(PATH)
@@ -33,6 +42,36 @@ def login(username: str, password: str):
     btn.click()
 
 
+def enroll_sc(target_ids):
+    # loop through targets
+    for target in target_ids:
+        print('TARGET: {}'.format(target))
+        # get list of course objects
+        dd_btn = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'multiselect__select')))
+        dd_btn.click()
+        sc_dropdown = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.multiselect__content')))
+        # sc_dropdown = driver.find_element_by_css_selector('ul.multiselect__content')
+        courses = sc_dropdown.find_elements_by_tag_name('li')
+
+        # extract course ids
+        courses_text = [c.text for c in courses]
+        courses_id = [c_text.split('\n')[1].split(' - ')[0] for c_text in courses_text if len(c_text) > 1]
+
+        # click on course
+        idx = courses_id.index(target)
+        print('Memilih course {}\n'.format(courses[idx].text))
+        courses[idx].click()
+
+        # TODO: pick schedule
+
+        # save course
+        save_btn = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'button.btn.btn-primary.btn-sm')))
+        save_btn.click()
+        time.sleep(1)
+
+
 if __name__ == '__main__':
     login(USERNAME, PASSWORD)
 
@@ -47,7 +86,8 @@ if __name__ == '__main__':
                 driver.refresh()
             except WebDriverException:
                 break
+        elif arg == 'p':
+            enroll_sc(COURSE_IDS)
 
     # time.sleep(5)
     # driver.quit()
-
