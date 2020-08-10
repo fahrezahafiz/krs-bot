cc22f832ee2d136d6bed8268f98e006a529688bc
Fahreza <fahreza_12@apps.ipb.ac.id>
2020-08-10 08:54:37 +0700

    clear creds

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import WebDriverException
 
 # constants
 # path to webdriver
-PATH = "C:\Program Files (x86)\msedgedriver.exe"
 BASE_URL = "https://krs.simak.ipb.ac.id"
-USERNAME = 'fahreza_12'
-PASSWORD = '279654066abc'
+PATH = 'C:\Program Files (x86)\msedgedriver.exe'
+USERNAME = ''
+PASSWORD = ''
 
 # initialize webdriver
 driver = webdriver.Edge(PATH)
@@ -22,17 +23,16 @@ def login(username: str, password: str):
     driver.get(BASE_URL)
 
     # login
-    username_field = driver.find_element_by_id('input-username')
-    username_field.send_keys(username)
-    password_field = driver.find_element_by_id('input-password')
-    password_field.send_keys(password, Keys.RETURN)
+    uname_field = driver.find_element_by_id('input-username')
+    uname_field.send_keys(username)
+    pwd_field = driver.find_element_by_id('input-password')
+    pwd_field.send_keys(password, Keys.RETURN)
 
     # wait for page to load then click "Isi KRS" button
-    btn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//a[@href="/krs"]')))
+    btn = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//a[@href="/krs"]')))
     btn.click()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     login(USERNAME, PASSWORD)
 
@@ -43,7 +43,10 @@ if __name__ == '__main__':
             driver.quit()
             break
         elif arg == 'r':
-            driver.refresh()
+            try:
+                driver.refresh()
+            except WebDriverException:
+                break
 
     # time.sleep(5)
     # driver.quit()
