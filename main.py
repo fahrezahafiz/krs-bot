8e1f6b54ce51ebe930b19c7962d3f82ae4bd9177
Fahreza <fahreza_12@apps.ipb.ac.id>
2020-08-10 13:44:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ BASE_URL = "https://krs.simak.ipb.ac.id"
 PATH = 'C:\Program Files (x86)\msedgedriver.exe'
 USERNAME = 'fahreza_12'
 PASSWORD = '279654066abc'
+# format: (
 COURSE_IDS = [
     'IKK233',
     'EKO201',
+    'TSL302',
 ]
 
 # initialize webdriver
@@ -45,21 +47,29 @@ def login(username: str, password: str):
 def enroll_sc(target_ids):
     # loop through targets
     for target in target_ids:
+        start_time = time.time()
         print('TARGET: {}'.format(target))
         # get list of course objects
-        dd_btn = WebDriverWait(driver, 10).until(
+        dd_btn = WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'multiselect__select')))
         dd_btn.click()
-        sc_dropdown = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.multiselect__content')))
+        sc_dropdown = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.multiselect__content')))
         # sc_dropdown = driver.find_element_by_css_selector('ul.multiselect__content')
         courses = sc_dropdown.find_elements_by_tag_name('li')
 
         # extract course ids
-        courses_text = [c.text for c in courses]
-        courses_id = [c_text.split('\n')[1].split(' - ')[0] for c_text in courses_text if len(c_text) > 1]
+        course_ids = []
+        for course in courses:
+            text = course.text
+            text = text.split('\n')
+            if len(text) > 1:
+                c_id = text[1].split(' - ')[0]
+                course_ids.append(c_id)
+            else:
+                course_ids.append(text)
 
         # click on course
-        idx = courses_id.index(target)
+        idx = course_ids.index(target)
         print('Memilih course {}\n'.format(courses[idx].text))
         courses[idx].click()
 
@@ -69,13 +79,13 @@ def enroll_sc(target_ids):
         save_btn = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, 'button.btn.btn-primary.btn-sm')))
         save_btn.click()
-        time.sleep(1)
+        print('Finished in {} seconds'.format(time.time() - start_time))
 
 
 if __name__ == '__main__':
     login(USERNAME, PASSWORD)
 
-    print('Commands\n(q: quit, r: reload)')
+    print('Commands\n(q: quit, r: reload, p: play)')
     while True:
         arg = input("> ")
         if arg == 'q':
